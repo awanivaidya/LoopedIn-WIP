3147aec2a9434757d35136c106f649d4253fedb3
awwony <awanivaidyawork@gmail.com>
2025-08-23 00:22:05 +0530

    JWT Token auth and returning user details IF authenticated

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
-
-router.get('/', (req, res) => {
-	console.log(req.body)
-	res.send('Auth route working')
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password')
+		res.json(user)
+	} catch (err) {
+		console.error(err.message)
+		res.status(500).send('Server error')
+	}
 })
 
 module.exports = router
