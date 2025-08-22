dbf1e9ca57d86d1162f3074f0b9c9e451b50c02f
awwony <awanivaidyawork@gmail.com>
2025-08-22 15:12:48 +0530

    Vali dation for user working

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
+
 //@desc Register Users
-router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('User route working')
-})
+router.post(
+	'/',
+	[
+		check('name', 'Name is required!').not().isEmpty(),
+		check('email', 'Please enter a valid email address').isEmail(),
+		check(
+			'password',
+			'Please enter a password with 8 or more characters'
+		).isLength({ min: 8 }),
+	],
+
+	(req, res) => {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+		console.log(req.body)
+		res.send('User route working')
+	}
+)
 
 module.exports = router
