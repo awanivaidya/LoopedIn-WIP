be1205709293bf4661715722dcd7030f7b5e151e
awwony <awanivaidyawork@gmail.com>
2025-08-23 04:03:07 +0530

    /me profile route working

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const auth = require('../../middleware/auth')
 const router = express.Router()
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
-router.get('/', (req, res) => {
-	console.log(req.body)
-	res.send('Profile route working')
+//@route		GET api/profile/me
+
+router.get('/me', auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id }).populate(
+			'user',
+			['name', 'avatar']
+		)
+
+		if (!profile) {
+			return res.status(400).json({ msg: 'There is no profile for this user' })
+		}
+		res.json(profile)
+	} catch (err) {
+		console.error(err.message)
+		res.status(500).send('Server Error!')
+	}
 })
 
 module.exports = router
