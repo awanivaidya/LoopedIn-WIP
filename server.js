7b996be55715556834da8128b5d3c3782611a4be
awwony <awanivaidyawork@gmail.com>
2025-09-11 00:01:37 +0530

    predeployment step

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
 const connectDB = require('./config/db')
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
 connectDB()
 
-app.use(express.json()) // no need for { extended: false } for JSON
+app.use(express.json())
 
-// CORS for your React app on 3000
 app.use(
 	cors({
 		origin: 'http://localhost:3000',
-		// credentials: true, // enable if you use cookies/sessions
 	})
 )
 
-// optional: explicitly handle preflight (cors() already does this, but this is fine)
 app.options(
 	'*',
 	cors({
 		origin: 'http://localhost:3000',
-		// credentials: true,
 	})
 )
 
-app.get('/', (req, res) => res.send('API Running'))
+//app.get('/', (req, res) => res.send('API Running')) =>remove for heroku deploy
 
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/user', require('./routes/api/user'))
@@ -33,4 +30,10 @@ app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 
 const PORT = process.env.PORT || 8080
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'))
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
